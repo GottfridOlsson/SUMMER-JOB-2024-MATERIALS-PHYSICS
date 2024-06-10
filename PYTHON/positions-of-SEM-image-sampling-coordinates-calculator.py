0ed1418e8beaafdd69270a61419e58f1a85a343e
Gottfrid Olsson <gottfridolsson@hotmail.com>
2024-06-10 17:07:54 +0200

    Update positions-of-SEM-image-sampling-coordinates-calculator.py

--- PYTHON/positions-of-SEM-image-sampling-coordinates-calculator.py
+++ PYTHON/positions-of-SEM-image-sampling-coordinates-calculator.py
@@ -12,14 +12,12 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import circle_fit
+import plot_functions as f
 
-
+f.set_LaTeX_and_CMU(True) #must run before plotting
 
 # VARIABLES #
-circle_radius = 1
 alpha = 1.05
-square_side_length = 0.1
-
 
 
 # FUNCTIONS #
@@ -33,7 +31,6 @@ def add_square_to_ax(side_length, x_pos, y_pos, ax):
 
 def add_circle_to_ax(radius, x_pos, y_pos, ax, edgecolor='gray', faceolor='gray', alpha=1):
     ax.add_artist(plt.Circle((x_pos,y_pos), radius=radius, edgecolor=edgecolor, facecolor=faceolor, alpha=alpha))
-
 
 def find_circle_center_and_radius_from_ABCD(A, B, C, D, view_fit=False):
     # A, B, C, D are tuples (x_i, y_i) of the coordinates of the SEM
@@ -50,7 +47,6 @@ def find_circle_center_and_radius_from_ABCD(A, B, C, D, view_fit=False):
         circle_fit.plot_data_circle(data, x_center_fit, y_center_fit, r_fit)
     return  x_center_fit, y_center_fit, r_fit, sigma_residual_fit
 
-
 def add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0_matrix, y0_matrix, distance_between_squares, ax):
     a = square_side_length
     d = distance_between_squares
@@ -58,7 +54,7 @@ def add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0_matrix, y0_matrix,
     x0 = x0_matrix
     y0 = y0_matrix
 
-    xy_coordinates_3_by_3_matrix_squares = []
+    xy_coordinates_3_by_3_matrix_squares_numbers = []
 
     counter = 1
     for i in range(0,3):
@@ -72,12 +68,11 @@ def add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0_matrix, y0_matrix,
             if j==2: deltaX = b
             add_square_to_ax(square_side_length, x0+deltaX, y0+deltaY, ax)
             ax.text(x0+deltaX, y0+deltaY+a*1.1/2, str(counter), horizontalalignment='center')
-            xy_coordinates_3_by_3_matrix_squares.append((x0+deltaX, y0+deltaY))
+            xy_coordinates_3_by_3_matrix_squares_numbers.append((counter, x0+deltaX, y0+deltaY))
             counter = counter + 1
         
-    return xy_coordinates_3_by_3_matrix_squares
+    return xy_coordinates_3_by_3_matrix_squares_numbers
         
-
 
 
 
@@ -94,32 +89,30 @@ x0 = x_center_fit
 y0 = y_center_fit
 print(f"Circle origo: ({x0:.2f}, {y0:.2f})\n")
 
+r_real = 2.5 # mm (the punched Li has diameter 5 mm, or r = 2.5 mm in reality)
+xAndy_distance_between_SEM_images = 1 # mm, say we want to have this distance (cartesian) between each square in the sampled matrix on the Cu
+distance_between_squares = (r_fit/r_real) * xAndy_distance_between_SEM_images # gives the same unit that r_fit is in
 square_side_length = r_fit/10
-xAndy_distance_between_SEM_images = r_fit/5
+
 
 
 # PLOT #
-figure, ax = plt.subplots()
-#add_circle_to_ax(circle_radius, 0, 0, ax, alpha=0.8)
-add_circle_to_ax(r_fit, x0, y0, ax, edgecolor='black', faceolor='gray', alpha=0.6)
+figure, axs = plt.subplots(1, 2, figsize=(8, 5))
+add_circle_to_ax(r_fit, x0, y0, axs[0], edgecolor='black', faceolor='gray', alpha=0.6)
+xy_coordinates_3_by_3_matrix_squares_numbers = add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0, y0, distance_between_squares, axs[0])
+for (i, x,y) in xy_coordinates_3_by_3_matrix_squares_numbers:
+    print(f"Position {i} of 9: ({x:.2f}, {y:.2f})")
 
-add_square_to_ax(square_side_length, x0, y0, ax)    
 
-xy_coordinates_3_by_3_matrix_squares = add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0, y0, xAndy_distance_between_SEM_images, ax)
-counter = 1
-for (x,y) in xy_coordinates_3_by_3_matrix_squares:
-    print(f"Position {counter} of 9: ({x:.2f}, {y:.2f})")
-    if counter in (3, 6, 9):
-        print("")
-    counter = counter + 1
-
-ax.set_aspect(1)
-ax.set_xlim(-alpha*r_fit, alpha*r_fit)
-ax.set_ylim(-alpha*r_fit, alpha*r_fit)
-ax.set_xlabel("$x$")
-ax.set_ylabel("$y$")
+axs[0].set_aspect(1)
+axs[0].set_xlim(-alpha*r_fit, alpha*r_fit)
+axs[0].set_ylim(-alpha*r_fit, alpha*r_fit)
+axs[0].set_xlabel("$x$")
+axs[0].set_ylabel("$y$")
 #ax.axis('off')
 
-
+axs[1].axis('tight')
+axs[1].axis('off')
+the_table = axs[1].table(xy_coordinates_3_by_3_matrix_squares_numbers, colLabels=('Square \\#', '$x$', '$y$'), fontsize=19, loc='center', edges='open', colLoc ='center', rowLoc='center', cellLoc='center')
 
 plt.show()
