eb313b0860f95f1fd18876a757fa7394a5dea2ed
Gottfrid Olsson <gottfridolsson@hotmail.com>
2024-06-12 16:58:27 +0200

    Update positions-of-SEM-image-sampling-coordinates-calculator.py

--- PYTHON/positions-of-SEM-image-sampling-coordinates-calculator.py
+++ PYTHON/positions-of-SEM-image-sampling-coordinates-calculator.py
@@ -33,6 +33,26 @@ def find_circle_center_and_radius_from_ABCD(points_along_circle, view_fit=False)
     if view_fit:
         circle_fit.plot_data_circle(points_along_circle, x_center_fit, y_center_fit, r_fit)
     return  x_center_fit, y_center_fit, r_fit, sigma_residual_fit
+def calculate_3_by_3_matrix_of_squares_from_center_coordinates(x_center, y_center, distance_between_squares, n_decimals=3):
+    d = distance_between_squares
+    x0 = x_center
+    y0 = y_center
+
+    xy_coordinates_3_by_3_matrix_squares = []
+    for i in range(0,3):
+        if i==0: deltaY = d
+        if i==1: deltaY = 0
+        if i==2: deltaY = -d
+
+        for j in range(0,3):
+            if j==0: deltaX = -d
+            if j==1: deltaX = 0
+            if j==2: deltaX = d
+            
+            center_position_square_ij = (round(x0+deltaX, n_decimals), round(y0+deltaY, n_decimals))
+            xy_coordinates_3_by_3_matrix_squares.append(center_position_square_ij)
+
+    return xy_coordinates_3_by_3_matrix_squares
 def add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0_matrix, y0_matrix, distance_between_squares, ax, n_decimals=3, draw_numbers=True, edgecolor='red', faceolor='none'):
     a = square_side_length
     d = distance_between_squares
@@ -58,27 +78,40 @@ def add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0_matrix, y0_matrix,
             counter = counter + 1
         
     return xy_coordinates_3_by_3_matrix_squares_numbers
-def calculate_3_by_3_matrix_of_squares_from_center_coordinates(x_center, y_center, distance_between_squares, n_decimals=3):
+def calculate_4_by_4_matrix_of_square_from_center_coordinates(x_center, y_center, distance_between_squares, n_decimals=3):
     d = distance_between_squares
     x0 = x_center
     y0 = y_center
 
-    xy_coordinates_3_by_3_matrix_squares_numbers = []
-    counter = 1
-    for i in range(0,3):
-        if i==0: deltaY = d
-        if i==1: deltaY = 0
-        if i==2: deltaY = -d
+    xy_coordinates_4_by_4_matrix_squares = []
 
-        for j in range(0,3):
-            if j==0: deltaX = -d
-            if j==1: deltaX = 0
-            if j==2: deltaX = d
+    for i in range(0, 4):
+        if i==0: deltaY =  3*d/2
+        if i==1: deltaY =  1*d/2
+        if i==2: deltaY = -1*d/2
+        if i==3: deltaY = -3*d/2
+        
+        for j in range(0, 4):
+            if j==0: deltaX = -3*d/2
+            if j==1: deltaX = -1*d/2
+            if j==2: deltaX =  1*d/2
+            if j==3: deltaX =  3*d/2
             
             center_position_square_ij = (round(x0+deltaX, n_decimals), round(y0+deltaY, n_decimals))
-            xy_coordinates_3_by_3_matrix_squares_numbers.append(center_position_square_ij)
+            xy_coordinates_4_by_4_matrix_squares.append(center_position_square_ij)
 
-    return xy_coordinates_3_by_3_matrix_squares_numbers
+    return xy_coordinates_4_by_4_matrix_squares
+def add_4_by_4_matrix_of_squares_to_ax(square_side_length, x0_matrix, y0_matrix, distance_between_squares, ax, n_decimals=3, draw_numbers=True, edgecolor='red', faceolor='none'):
+    xy_coordinates_4_by_4_matrix_squares = calculate_4_by_4_matrix_of_square_from_center_coordinates(x0_matrix, y0_matrix, distance_between_squares, n_decimals=n_decimals)
+
+    xy_coordinates_4_by_4_matrix_squares_numbers = []
+    for i in range(0, 16):
+        add_square_to_ax(square_side_length, xy_coordinates_4_by_4_matrix_squares[i][0], xy_coordinates_4_by_4_matrix_squares[i][1], ax, edgecolor=edgecolor, faceolor=faceolor)
+        if draw_numbers:
+            ax.text(xy_coordinates_4_by_4_matrix_squares[i][0], xy_coordinates_4_by_4_matrix_squares[i][1]+square_side_length*1.1/2, str(i+1), horizontalalignment='center')
+            xy_coordinates_4_by_4_matrix_squares_numbers.append((i+1, round(xy_coordinates_4_by_4_matrix_squares[i][0], n_decimals), round(xy_coordinates_4_by_4_matrix_squares[i][1], n_decimals)))
+
+    return xy_coordinates_4_by_4_matrix_squares_numbers
 def get_current_date_and_time_as_ISO8601_string():
     import datetime
     date_and_time = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
@@ -126,19 +159,30 @@ add_circle_to_ax(r_fit, x0, y0, axs[0], edgecolor='black', faceolor='gray', alph
     
 if include_AI_squares: # Squares for SEM images to train AI (optional)
     xy_coordinates_3_by_3_matrix_squares_numbers_AI_squares = add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0, y0, distance_between_squares_AI_training, axs[0], n_decimals=3, draw_numbers=0, edgecolor='k')
-    for (i, x,y) in xy_coordinates_3_by_3_matrix_squares_numbers_AI_squares:
+    for (i, x, y) in xy_coordinates_3_by_3_matrix_squares_numbers_AI_squares:
         if i==1:
             print(f"Positions for the squares for images to train AI:")
         print(f"Position {i} of 9: ({x:.3f}, {y:.3f})")
 
+
 # Squares to take SEM images for analysis
 xy_coordinates_3_by_3_matrix_squares_numbers = add_3_by_3_matrix_of_squares_to_ax(square_side_length, x0, y0, distance_between_squares, axs[0], n_decimals=3)
 axs[1].table(xy_coordinates_3_by_3_matrix_squares_numbers, colLabels=('Square \\#', '$x$', '$y$'), fontsize=19, loc='center', edges='open', colLoc ='center', rowLoc='center', cellLoc='center')
-for (i, x,y) in xy_coordinates_3_by_3_matrix_squares_numbers:
+for (i, x, y) in xy_coordinates_3_by_3_matrix_squares_numbers:
     if i==1: print(f"\nPositions for the squares for analysis of SEM images:")
     print(f"Position {i} of 9: ({x:.3f}, {y:.3f})")
 
-
+'''
+distance_between_squared_4_by_4 = 0.8 #mm
+distance_between_squares_4_by_4_SEM = (r_fit/r_real) * distance_between_squared_4_by_4 #in SEM coordinate system
+xy_coordinates_4_by_4_matrix_squares_numbers = add_4_by_4_matrix_of_squares_to_ax(square_side_length, x0, y0, distance_between_squares_4_by_4_SEM, axs[0], n_decimals=3)
+axs[1].table(xy_coordinates_4_by_4_matrix_squares_numbers, colLabels=('Square \\#', '$x$', '$y$'), fontsize=19, loc='center', edges='open', colLoc ='center', rowLoc='center', cellLoc='center')
+for (i, x, y) in xy_coordinates_4_by_4_matrix_squares_numbers:
+    if i==1: print(f"\nPositions for the squares for analysis of SEM images:")
+    print(f"Position {i} of 16: ({x:.3f}, {y:.3f})")
+'''
+    
+    
 ## if there is a problem clearly seeing where the points A, B, C, and D is, take a point that is at the center of the Li-deposition and put that in as x0, y0 below
 '''
 Li_center_x = -9.85 
